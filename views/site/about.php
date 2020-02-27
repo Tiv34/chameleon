b0b0d72e2722ee7e2182dc423eeb4a34f05369c7
Lev <slarktiv34@gmail.com>
2020-02-27 20:46:32 +0300

    img about

--- views/site/about.php
+++ views/site/about.php
@@ -10,6 +10,21 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="container container_conntent">
     <div class="site-about">
         <h1><?= Html::encode($this->title) ?></h1>
-        <img src="../../web/images/waituserCMS.jpg" width="100%" height="100%"/>
     </div>
+
+    <div class="row">
+        <div class="col-xs-6">
+            <p>
+                Миссия компании «itp-lab» выходит далеко за рамки предоставления технологических решений.
+                Мы помогаем максимально эффективно выстроить процессы, чтобы клиенты могли сосредоточить усилия на самом
+                важном — своей основной деятельности.
+                Располагая полным спектром услуг, мы заботимся о том, чтобы вы сохраняли преимущество в непрерывно
+                меняющейся технологической среде и успешно достигали своих целей.
+            </p>
+        </div>
+        <div class="col-xs-4">
+            <img src="/web/images/about.jpg" width="500" height="400" style="margin-bottom: 100px">
+        </div>
+    </div>
+</div>
 </div>
