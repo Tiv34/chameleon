c2430eedd5aa91e8f6333de6e9b332894ff9ecd0
lev <slarktiv34@gmail.com>
2020-03-09 11:25:27 +0300

    css

--- views/site/about.php
+++ views/site/about.php
@@ -7,15 +7,15 @@ use yii\helpers\Html;
 $this->title = 'О нас';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<img class="fonts_back" src="/web/images/fonts_back3.png">
+<!--<img class="fonts_back" src="/web/images/fonts_back3.png">-->
 
 <div class="container container_conntent">
+    <div id="about_container">
     <div class="site-about">
         <h1><?= Html::encode($this->title) ?></h1>
     </div>
-
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-8">
             <p>
                 Миссия компании «itp-lab» выходит далеко за рамки предоставления технологических решений.
                 Мы помогаем максимально эффективно выстроить процессы, чтобы клиенты могли сосредоточить усилия на самом
@@ -24,9 +24,10 @@ $this->params['breadcrumbs'][] = $this->title;
                 меняющейся технологической среде и успешно достигали своих целей.
             </p>
         </div>
-        <div class="col-xs-4">
-            <img src="/web/images/about.jpg" width="500" height="400" style="margin-bottom: 100px">
+        <div class="col-xs-2">
+            <img src="/web/images/about_start.png" width="300" height="300">
         </div>
     </div>
+    </div>
 </div>
-</div>
+
