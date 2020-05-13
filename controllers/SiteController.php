c8cf76499524575428c222931d0182ba4774f366
tiv34 <slarktiv34@gmail.com>
2020-05-13 17:02:53 +0300

    css index container fix

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -115,23 +115,12 @@ class SiteController extends Controller
     {
         return $this->render('outsourcing');
     }
-    public function actionColor1()
+    public function actionDevice()
     {
-        return $this->render('color1');
+        return $this->render('device');
     }
     public function actionReporting()
     {
         return $this->render('reporting');
-    }
-    public function actionColor3()
-    {
-        $model = new ContactForm();
-        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
-            Yii::$app->session->setFlash('contactFormSubmitted');
-            return $this->refresh();
-        }
-        return $this->render('index', [
-            'model' => $model,
-        ]);
     }
 }
